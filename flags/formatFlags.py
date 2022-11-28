7a0738176400d0813bd639a73367824be9622d77
gjorsi <carl.august.g@gmail.com>
2022-11-28 10:31:39 +0100

    Finished flag generator

--- flags/formatFlags.py
+++ flags/formatFlags.py
@@ -1,4 +1,5 @@
 import base64
+import json
 
 with open('flags/raw.txt') as f:
     rawText = f.readline()
@@ -10,13 +11,21 @@ scrubbed = ''.join(filter(whitelist.__contains__, rawText))
 wordsArray = scrubbed.lower().split(' ')
 wordCount = 3
 phrases = ["_".join(wordsArray[i:i+wordCount]) for i in range(0, len(wordsArray), wordCount)]
-print(phrases[-5])
 
-base64list = [base64.b64encode(item.encode('ascii')) for item in phrases]
-print(base64list[-5])
+base64list = [base64.b64encode(item.encode('ascii')).decode('ascii') for item in phrases]
+print(len(base64list))
 
-print(phrases[5])
-print(phrases[9])
-print(phrases[13])
-print(phrases[18])
+print(base64list[23]) #became_the_first
 print(phrases[23])
+print(base64list[1]) #decentralized_digital_currency
+print(phrases[1])
+print(base64list[56]) #by_satoshi_nakamoto
+print(phrases[56])
+print(base64list[59]) #through_public_key
+print(phrases[59])
+print(base64list[93]) #barrier_to_participation
+print(phrases[93])
+
+jsonStr = json.dumps(base64list[:100])
+# print([f'{v} {i}' for i, v in enumerate(phrases[:100])])
+print(jsonStr)
